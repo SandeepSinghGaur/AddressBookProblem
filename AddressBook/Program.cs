239207751c4cbcc9b6e21f02018423c03f98e42e
SandeepSinghGaur <sandeepgaurdec13@gmail.com>
2021-01-29 17:35:33 +0530

    [Sandeep] Added:Create Contact in Address Book

---
+++ AddressBook/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Address Book System !");
+            Console.WriteLine("Create Contact in Address Book");
+            Console.WriteLine("Enter the Name of person:");
+            String name = Console.ReadLine();
+            Console.WriteLine("Enter the address");
+            String address = Console.ReadLine();
+            Console.WriteLine("Enter the city");
+            String city = Console.ReadLine();
+            Console.WriteLine("Enter the state");
+            String state = Console.ReadLine();
+            Console.WriteLine("Enter the zip");
+            String zip = Console.ReadLine();
+            Console.WriteLine("Enter the phone number");
+            String phoneNumber = Console.ReadLine();
+        }
+    }
+}
