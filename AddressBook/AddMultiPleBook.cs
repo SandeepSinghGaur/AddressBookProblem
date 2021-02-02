61092a135d42d19dfab2cc62eae9e081b216bed6
SandeepSinghGaur <sandeepgaurdec13@gmail.com>
2021-02-02 09:13:14 +0530

    [Sandeep] Refactor:Add Multiple AddressBook Each has Unique Name

---
+++ AddressBook/AddMultiPleBook.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+    class AddMultiPleBook
+    {
+        Dictionary<string, AddressBookOperation> addBookinDic = new Dictionary<string, AddressBookOperation>();
+
+        public void OpenAddressBook()
+        {
+            Console.WriteLine("Address Book We Have Created");
+            Console.WriteLine("Enter name of Address Book which we want open");
+            string bookName = Console.ReadLine();
+            Boolean keyPresent = addBookinDic.ContainsKey(bookName);
+            if (keyPresent)
+            {
+                AddressBookOperation addressBook = addBookinDic[bookName];
+                Console.WriteLine("Address Book:\n"
+                + "1) Add Person\n"
+                + "2) Edit Person\n"
+                + "3) Display\n"
+                + "4) Delete Person\n"
+                + "5) Close");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        AddressBookOperation.Add_Contact();
+                        break;
+                    case 2:
+                        AddressBookOperation.Edit_Contact();
+                        break;
+                    case 3:
+                        AddressBookOperation.DisplayAddressBook();
+                        break;
+                    case 4:
+                        AddressBookOperation.Delete_Contact();
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter Correct Choice");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Address Book is not present");
+            }
+        }
+        public void NewAddressBook()
+        {
+            AddressBookOperation addressBook = new AddressBookOperation();
+            Console.WriteLine("Enter name of address book");
+            String bookName = Console.ReadLine();
+            bool keyPresent = addBookinDic.ContainsKey(bookName);
+            if (keyPresent)
+                Console.WriteLine("Already Present");
+            else
+                addBookinDic.Add(bookName, addressBook);
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -10,36 +10,58 @@ namespace AddressBook
             //AddressBookOperation.Add_Contact();
             //AddressBookOperation.Edit_Contact();
             //AddressBookOperation.Delete_Contact();
-            int checkOut = 0;
-            AddressBookOperation object1= new AddressBookOperation();
-            do
-            {
-                Console.WriteLine("Enter Your choice 1: Add Person Details ");
-                Console.WriteLine("Enter Your choice 2: Editing Person Details ");
-                Console.WriteLine("Enter Your choice 3: Delete  Person Details ");
-                Console.WriteLine("Enter Your choice 4: To exist ");
+            // int checkOut = 0;
+            //AddressBookOperation object1= new AddressBookOperation();
+            //do
+            //{
+            //    Console.WriteLine("Enter Your choice 1: Add Person Details ");
+            //    Console.WriteLine("Enter Your choice 2: Editing Person Details ");
+            //    Console.WriteLine("Enter Your choice 3: Delete  Person Details ");
+            //    Console.WriteLine("Enter Your choice 4: To exist ");
 
-                int choice = Convert.ToInt32(Console.ReadLine());
-                switch (choice)
-                {
-                    case 1:
-                        AddressBookOperation.Add_Contact();
-                        AddressBookOperation.DisplayAddressBook();
-                        break;
-                    case 2:
-                        AddressBookOperation.Edit_Contact();
-                        AddressBookOperation.DisplayAddressBook();
-                        break;
-                    case 3:
-                        object1.DeletPerson();
-                        AddressBookOperation.DisplayAddressBook();
-                        break;
-                    default:
-                        checkOut = 1;
-                        break;
-                }
-            } while (checkOut == 0);
-            AddressBookOperation.DisplayAddressBook();
+            //    int choice = Convert.ToInt32(Console.ReadLine());
+            //    switch (choice)
+            //    {
+            //        case 1:
+            //            AddressBookOperation.Add_Contact();
+            //            AddressBookOperation.DisplayAddressBook();
+            //            break;
+            //        case 2:
+            //            AddressBookOperation.Edit_Contact();
+            //            AddressBookOperation.DisplayAddressBook();
+            //            break;
+            //        case 3:
+            //            object1.DeletPerson();
+            //            AddressBookOperation.DisplayAddressBook();
+            //            break;
+            //        default:
+            //            checkOut = 1;
+            //            break;
+            //    }
+            //} while (checkOut == 0);
+            //AddressBookOperation.DisplayAddressBook();
+            AddMultiPleBook addBook = new AddMultiPleBook();
+            bool condition = true;
+            Console.WriteLine("Welcome to Addressbook");
+            while (condition)
+            {
+                condition = false;
+                Console.WriteLine("Multiple Address Book:\n"
+                        + "1) open Existing Address book\n"
+                        + "2) Create new address book\n"
+                        + "3) Close \n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                if (option == 1)
+                    addBook.OpenAddressBook();
+                else if (option == 2)
+                    addBook.NewAddressBook();
+                else if (option == 3)
+                    break;
+                else
+                    Console.WriteLine("Enter Correct option");
+
+            }
+            Console.WriteLine("Thank You");
 
         }
     }
